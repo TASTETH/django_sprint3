c47c4b3bf274ec3104a7d4bc85d3b01a6a97cdc8
Ruslan Dzarasov <ruslandzarasov835@gmail.com>
2025-05-23 00:27:19 +0300

    asdasd

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogicum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'html'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
